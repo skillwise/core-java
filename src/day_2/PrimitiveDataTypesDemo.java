b67c574d30995b17f250ab0f4e56d7c791c502dc
skillwise <skillwise.edu@gmail.com>
2025-08-07 23:04:27 +0530

    primitive datatype in java

---
+++ src/day_2/PrimitiveDataTypesDemo.java
@@ -0,0 +1,24 @@
+package day_2;
+
+public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        // Primitive data types in Java
+        byte b = 10;
+        short s = 1000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 10.5f;
+        double d = 20.123456;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + bool);
+    }
+}
